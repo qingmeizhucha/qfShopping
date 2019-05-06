3cdcfc6e0295c78f323ef6298c1c398a42c11e3f
qingmeizhucha <209691440@qq.com>
2019-05-06 15:08:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mryan/qfshopping/app/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.mryan.qfshopping.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mryan.qfshopping.R;
+
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcom_activity);
+    }
+}
